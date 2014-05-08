2bd0582f1229eae1eb3606284a1a91b3be3cd302
David E. Shere <david.e.shere@gmail.com>
2014-05-08 15:10:04 -0700

    Added new python live rotation.  Started working on C++ live rotation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <gflags/gflags.h>
 #include <cstdlib>
+#include <list>
 
 using namespace std;
 using namespace cv;
@@ -11,6 +12,7 @@ DEFINE_bool(display, true, "Display video in window.");
 DEFINE_bool(quiet, false, "Suppress terminal output.");
 DEFINE_string(save, "", "Save output to file.");
 DEFINE_string(axis, "y", "Axis of rotation.");
+DEFINE_bool(live, false, "Use live feed.");
 
 enum axis_enum {
     AXIS_X,
@@ -24,21 +26,32 @@ typedef struct options_struct {
     int frame_count;
     VideoCapture *video_src;
     VideoWriter *video_dst;
+    bool live;
 } options_type;
+
+int video_file_filter(options_type *o, char *window_name);
+int video_live_filter(options_type *o, char *window_name);
 
 options_type * create_options(int *argc, char ***argv) {
     google::ParseCommandLineFlags(argc, argv, true);
     options_type *o = (options_type *) calloc(1, sizeof(options_type));
     
-    if (*argc != 2) {
+    if (FLAGS_live && *argc == 1) {
+        o->live = true;
+    } else if (*argc != 2) {
+        o->live = false;
         cout << "usage: " << *argv[0] << " input-video" << endl;
         free(o);
         return NULL;
     }
 
     // open input video
-    string input_file = (*argv)[(*argc) - 1];
-    o->video_src = new VideoCapture(input_file);
+    if (o->live) {
+        o->video_src = new VideoCapture(0);
+    } else {
+        string input_file = (*argv)[(*argc) - 1];
+        o->video_src = new VideoCapture(input_file);
+    }
     if(!o->video_src->isOpened()) return NULL;
 
     // get axis
@@ -57,7 +70,10 @@ options_type * create_options(int *argc, char ***argv) {
     int width = o->video_src->get(CV_CAP_PROP_FRAME_WIDTH);
     int height = o->video_src->get(CV_CAP_PROP_FRAME_HEIGHT);
     int frame_count = o->video_src->get(CV_CAP_PROP_FRAME_COUNT);
-    if (FLAGS_axis == "x") {
+    if (o->live) {
+        o->size = Size(width, height);
+        o->frame_count = FLAGS_axis == "x" ? height : width;
+    } else if (FLAGS_axis == "x") {
         o->size = Size(width, frame_count);
         o->frame_count = height;
     } else if (FLAGS_axis == "y") {
@@ -85,15 +101,70 @@ options_type * create_options(int *argc, char ***argv) {
 int main(int argc, char **argv) {
 
     options_type *o = create_options(&argc, &argv);
-
-    Mat frame_in;
-    *(o->video_src) >> frame_in;
-    Mat frame_out(o->size, frame_in.type());
+    if (o == NULL)
+        return -1;
 
     if (FLAGS_display) {
         namedWindow(argv[0],CV_WINDOW_NORMAL);
         setWindowProperty(argv[0], CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
     }
+
+    if (o->live)
+        video_live_filter(o, argv[0]);
+    else
+        video_file_filter(o, argv[0]);
+
+    delete o->video_src;
+    delete o->video_dst;
+    free(o);
+
+    return 0;
+}
+
+int video_live_filter(options_type *o, char *window_name) {
+    Mat frame_in;
+    *(o->video_src) >> frame_in;
+    Mat frame_out(o->size, frame_in.type());
+
+    list<Mat> frame_list;
+    for (int i = 0; i < o->frame_count; i++) {
+        cout << "Frame " << i << " of " << o->frame_count << endl;
+        *(o->video_src) >> frame_in;
+        frame_list.push_back(frame_in);
+    }
+
+    int j = 0;
+    int d = 1;
+    for (;;) {
+        int i = 0;
+        for (list<Mat>::iterator it = frame_list.begin(); it != frame_list.end(); it++) {
+            switch (o->axis) {
+                case AXIS_X:
+                    it->col(j).copyTo(frame_out.col(i++));
+                    break;
+                case AXIS_Y:
+                    it->col(j).copyTo(frame_out.col(i++));
+                    break;
+                default:
+                    cout << "UNIMPLEMENTED" << endl;
+                    return -1;
+            }
+        }
+        imshow(window_name, frame_out);
+        waitKey(30);
+
+        *(o->video_src) >> frame_in;
+        frame_list.push_back(frame_in);
+        frame_list.pop_front();
+    }
+        
+
+}
+
+int video_file_filter(options_type *o, char *window_name) {
+    Mat frame_in;
+    *(o->video_src) >> frame_in;
+    Mat frame_out(o->size, frame_in.type());
 
     int frame_count_src = o->video_src->get(CV_CAP_PROP_FRAME_COUNT);
     for (int j = 0; j < o->frame_count; j++) {
@@ -121,7 +192,7 @@ int main(int argc, char **argv) {
 
         // Display window
         if (FLAGS_display) {
-            imshow(argv[0], frame_out);
+            imshow(window_name, frame_out);
             waitKey(30);
         }
 
@@ -130,10 +201,4 @@ int main(int argc, char **argv) {
             o->video_dst->write(frame_out);
 
     }
-
-    delete o->video_src;
-    delete o->video_dst;
-    free(o);
-
-    return 0;
 }
